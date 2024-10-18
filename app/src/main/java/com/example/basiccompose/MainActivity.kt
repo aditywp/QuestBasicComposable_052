9752460d4c4aec1861065437c8dd62709b2893a0
adityapermono8 <adityapermono8@gmail.com>
2024-10-18 14:36:29 +0700

    mengubah fontsize dan fontnya

--- app/src/main/java/com/example/basiccompose/MainActivity.kt
+++ app/src/main/java/com/example/basiccompose/MainActivity.kt
@@ -9,15 +9,20 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.Role
-import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.basiccompose.ui.theme.BasicComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -66,6 +71,10 @@ fun BasicCompose(adit: Modifier = Modifier) {
         )
 
         Text(text = "Nama",
+            style = TextStyle(
+                fontSize = 20.sp,
+                fontStyle = FontStyle.Italic
+            )
         )
     }
 
