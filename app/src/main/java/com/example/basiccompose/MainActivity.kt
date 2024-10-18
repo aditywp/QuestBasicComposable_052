05281972da414b92854e98dc0a525565fac9f225
adityapermono8 <adityapermono8@gmail.com>
2024-10-18 14:47:13 +0700

    Mengubah foto menjadi rounded pada sudunya 200.dp

--- app/src/main/java/com/example/basiccompose/MainActivity.kt
+++ app/src/main/java/com/example/basiccompose/MainActivity.kt
@@ -10,11 +10,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -95,7 +97,11 @@ fun BasicCompose(adit: Modifier = Modifier) {
 
         Image(painter = painterResource(id = R.drawable.krs),
             contentDescription = null,
-            modifier = Modifier.size(300.dp)
+            modifier = Modifier
+                .size(300.dp)
+                .clip(RoundedCornerShape(200.dp))
+
+
         )
 
     }
